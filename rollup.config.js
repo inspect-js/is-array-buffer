0aa4681fa71d93297c92cdefc25df995cc83497c
Chen Fengyuan <chenfengyuan@outlook.com>
2018-04-01 15:13:56 +0800

    build: update

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,7 @@ const babel = require('rollup-plugin-babel');
 const pkg = require('./package');
 
 const now = new Date();
-
-export default {
-  entry: 'src/index.js',
-  targets: [
-    {
-      dest: 'dist/is-array-buffer.js',
-    },
-    {
-      dest: 'dist/is-array-buffer.common.js',
-      format: 'cjs',
-    },
-    {
-      dest: 'dist/is-array-buffer.esm.js',
-      format: 'es',
-    },
-  ],
-  format: 'umd',
-  moduleName: 'isArrayBuffer',
-  plugins: [
-    babel({
-      exclude: '/node_modules/**',
-    }),
-  ],
-  banner: `/*!
+const banner = `/*!
  * isArrayBuffer v${pkg.version}
  * https://github.com/fengyuanchen/${pkg.name}
  *
@@ -34,5 +11,31 @@ export default {
  *
  * Date: ${now.toISOString()}
  */
-`,
+`;
+
+export default {
+  entry: 'src/index.js',
+  targets: [
+    {
+      banner,
+      dest: 'dist/is-array-buffer.js',
+      format: 'umd',
+      name: 'isArrayBuffer',
+    },
+    {
+      banner,
+      dest: 'dist/is-array-buffer.common.js',
+      format: 'cjs',
+    },
+    {
+      banner,
+      dest: 'dist/is-array-buffer.esm.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    babel({
+      exclude: '/node_modules/**',
+    }),
+  ],
 };
