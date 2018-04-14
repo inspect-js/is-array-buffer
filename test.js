066cc1d00cf2cbd9975aef9e60df7d71c3c8f5e1
Chen Fengyuan <chenfengyuan@outlook.com>
2018-04-14 20:13:54 +0800

    test: group test suites

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
 const isArrayBuffer = require('./');
 
-it('should return true if it is an ArrayBuffer', () => {
-  assert(isArrayBuffer(new ArrayBuffer()));
-});
+describe('is-array-buffer', () => {
+  it('should return true if it is an ArrayBuffer', () => {
+    assert(isArrayBuffer(new ArrayBuffer()));
+  });
 
-it('should return false if it is not an ArrayBuffer', () => {
-  assert(!isArrayBuffer([]));
+  it('should return false if it is not an ArrayBuffer', () => {
+    assert(!isArrayBuffer([]));
+  });
 });
